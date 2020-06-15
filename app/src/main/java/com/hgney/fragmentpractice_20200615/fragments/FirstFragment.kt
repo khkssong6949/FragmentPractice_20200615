2f8f75cb533e6f10392f492d1f0f117ac43f01b8
khkssong6949 <bada6949@gmial.com>
2020-06-15 19:19:18 +0900

    프래그먼트 생성 완료 -> 화면에는 올리지 않은 상태

--- app/src/main/java/com/hgney/fragmentpractice_20200615/fragments/FirstFragment.kt
+++ app/src/main/java/com/hgney/fragmentpractice_20200615/fragments/FirstFragment.kt
@@ -1,7 +1,27 @@
 package com.hgney.fragmentpractice_20200615.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hgney.fragmentpractice_20200615.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        messageTxt.text = "안녕하세요"
+    }
 
 }
